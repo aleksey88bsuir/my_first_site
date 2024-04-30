ac9f938e8556223d37148cb7bb644c53f7d53067
aleksey88bsuir <aleksey88bsuir@yandex.ru>
2024-04-30 15:08:56 +0300

    создал модель БД

--- simple_site/articles/models.py
+++ simple_site/articles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=30)
+    content = models.TextField(blank=True)
+    photo = models.ImageField(upload_to='photos')
+    is_published = models.BooleanField(default=True)
