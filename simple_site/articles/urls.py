5785bc8f3e92892f137a8b697ff221910689963b
aleksey88bsuir <aleksey88bsuir@yandex.ru>
2024-04-30 06:24:41 +0300

    Внес необходимые изменения в ДБ для работы с базой MySQL

--- simple_site/articles/urls.py
+++ simple_site/articles/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', start_page),
     path('all', display_all_articles, name='main_page'),
     path('all/<int:article_id>/', article_by_id, name='article_by_id'),
-    path('all/<str:article_slug>/', article_by_slug, name='article_by_slug'),
+    path('all/<slug:article_slug>/', article_by_slug, name='article_by_slug'),
 ]
--- simple_site/simple_site/settings.py
+++ simple_site/simple_site/settings.py
@@ -81,6 +81,16 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'articles.db',
+#         'USER': 'aleksey88bsuir',
+#         'PASSWORD': 'password',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
