0a8b5cc83d1da32cbfcadd2c23ffc632deb3467a
aleksey88bsuir <aleksey88bsuir@yandex.ru>
2024-05-01 20:30:30 +0300

    Сделал поиск по сайту

--- simple_site/articles/urls.py
+++ simple_site/articles/urls.py
@@ -3,9 +3,10 @@ from .views import *
 
 
 urlpatterns = [
-    path('', start_page),
+    path('', start_page, name='start_page'),
     path('about', about, name='about'),
-    path('all', display_all_articles, name='main_page'),
-    path('all/<int:article_id>/', article_by_id, name='article_by_id'),
-    path('all/<slug:article_slug>/', article_by_slug, name='article_by_slug'),
+    path('contact', contact, name='contact'),
+    path('login', login, name='login'),
+    path('post/<int:post_id>/', show_post, name='post'),
+    path('search', get_queryset, name='search'),
 ]
--- simple_site/articles/views.py
+++ simple_site/articles/views.py
@@ -1,31 +1,52 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Article
+from django.db.models import Q
 # Create your views here.
 
-menu = ['Главная', 'Обратная связь', 'О сайте', 'Регистрация']
+menu = [
+    {'title': 'О сайте', 'url_name': 'about'},
+    {'title': 'Обратная связь', 'url_name': 'contact'},
+    {'title': 'Войти', 'url_name': 'login'},
+]
 
 
-def start_page(reguest):
+def start_page(request):
     published_articles = Article.objects.filter(is_published=True)
-
-    return render(reguest, "articles/index.html",
-                  {'menu': menu,
-                   'title': 'Главная страница',
-                   'posts': published_articles})
-
-
-def about(reguest):
-    return render(reguest, "articles/about.html", {'menu': menu, 'title': 'О сайте'})
+    context = {'menu': menu,
+               'title': 'Главная страница',
+               'posts': published_articles,
+               }
+    return render(request, "articles/index.html", context=context)
 
 
-def display_all_articles(request):
-    return HttpResponse('<h1>Отображение всех статей</h1>')
+def about(request):
+    return render(request, "articles/about.html", {'menu': menu, 'title': 'О сайте'})
 
 
-def article_by_id(request, article_id):
-    return HttpResponse(f'<h1>Отображение всех статей</h1><p>{article_id}</p>')
+def contact(request):
+    return render(request, "articles/about.html", {'menu': menu, 'title': 'Контакты'})
 
 
-def article_by_slug(request, article_slug):
-    return HttpResponse(f'<h1>Отображение всех статей</h1><p>{article_slug}</p>')
+def login(request):
+    return render(request, "articles/about.html", {'menu': menu, 'title': 'Регистрация'})
+
+
+def show_post(request, post_id):
+    post = Article.objects.get(pk=post_id)
+    context = {'menu': menu,
+               'title': 'Статья',
+               'post': post}
+    return render(request, "articles/post.html", context=context)
+
+
+def get_queryset(request):
+    search_param = request.GET.get('q')
+    search_query = Q(title__icontains=search_param) | Q(author__icontains=search_param) | Q(
+        content__icontains=search_param) & Q(is_published=True)
+    published_articles = Article.objects.filter(search_query)
+    context = {'menu': menu,
+               'title': 'Главная страница',
+               'posts': published_articles,
+               }
+    return render(request, "articles/index.html", context=context)
