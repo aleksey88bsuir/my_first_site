b4a54e52a15368ae3b0f204efbf09e737211f4f8
aleksey88bsuir <aleksey88bsuir@yandex.ru>
2024-05-08 15:12:54 +0300

    Поменял в очередной раз css файл, поместил js в папку static

--- simple_site/articles/static/articles/js/button_animation.js
+++ simple_site/articles/static/articles/js/button_animation.js
@@ -1,13 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var buttons = document.querySelectorAll('.animated-button');
 
-    buttons.forEach(function(button) {
+    var buttons = document.querySelectorAll('.animated-button');
+    Array.from(buttons).forEach(function(button) {
         button.addEventListener('mouseover', function() {
-            this.style.backgroundColor = 'white'; // Цвет при наведении
+            button.style.backgroundColor = 'red'; // Цвет при наведении
         });
 
         button.addEventListener('mouseout', function() {
-            this.style.backgroundColor = ''; // Возвращение к исходному цвету
+            button.style.backgroundColor = 'yellow'; // Возвращение к исходному цвету
         });
     });
-});
